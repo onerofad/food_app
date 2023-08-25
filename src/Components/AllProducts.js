d271b5886363e92c48ef7d19f173094cc14e6f21
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-08-25 01:22:31 +0100

    first commit

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -40,10 +40,10 @@ const AllProducts = ({mobile}) => {
                if(product.prodId >= firstItemId && product.prodId <= lastItemId){
                 return(
                     <Grid.Column computer={4} mobile={8} tablet={8}>
-                        <Segment raised style={{}} >
+                        <Segment raised style={{minHeight: mobile ? 300 : 330}} >
                             <Image style={{cursor: 'pointer'}} onClick ={() => {handleBtnProduct(product.prodId,product.name.replaceAll(" ", "-"))}} src={product.image} centered />
                             <Message color="teal" size="mini">
-                                <Message.Header style={{cursor: '', fontSize: mobile ? '0.8em' : ''}} onClick ={() => {handleBtnProduct(product.prodId,product.name.replaceAll(" ", "-"))}}>{product.name}</Message.Header>
+                                <Message.Header style={{cursor: '', fontSize: mobile ? '1.2em' : ''}} onClick ={() => {handleBtnProduct(product.prodId,product.name.replaceAll(" ", "-"))}}>{product.name}</Message.Header>
                             </Message>
                         </Segment>
                         <br/>
